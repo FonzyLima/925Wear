f487917a5ead1c71bae3d26840acda483ffb0377
FonzyLima <Alfonso_lima@dlsu.edu.ph>
2023-10-03 01:28:30 +0800

    Padding

--- src/components/About/Story.tsx
+++ src/components/About/Story.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const Story = () => {
   return (
-    <div className="w-full px-[60px] py-[55px]">
+    <div className="w-full px-[60px] py-[40px]">
       <div className="flex flex-col space-y-10 px-[60px] py-[55px] border-solid border rounded-[30px]">
         <div className="text-[60px] font-primary">Our Story</div>
         <div className="flex flex-row justify-between mt-[30px]">
