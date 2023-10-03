0e8c3f624ee36b1c87e39937eb7e6746650dd5b9
Leigh Arriane Buendia <81524834+leigharriane@users.noreply.github.com>
2023-10-03 23:35:48 +0800

    carousel changes

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import sample from "@/assets/sample.png";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -9,22 +9,9 @@ const Header = () => {
   return (
     <>
       <div className="w-full pt-[95px] px-[40px] bg-white font-primary">
-        <div className="w-full rounded-[30px] flex flex-col justify-center relative overflow-hidden h-[500px]">
-          {/* <Image
-            src={sample}
-            alt="header"
-            objectFit="cover"
-            className="absolute top-0 z-0 h-full object-cover"
-          />
-            <Image
-              src={sample}
-              alt="header"
-              objectFit="cover"
-              className="absolute top-0 z-0 h-full object-cover"
-            /> */}
-
+        <div className="w-full flex justify-center relative overflow-hidden  rounded-[30px]">
           <Carousel
-            className="absolute"
+            className=""
             autoPlay={true}
             interval={4000}
             infiniteLoop={true}
@@ -43,7 +30,9 @@ const Header = () => {
                   role="button"
                   tabIndex={0}
                   aria-label={`${label} ${index + 1}`}
-                ></span>
+                >
+                  .
+                </span>
               );
             }}
             renderArrowNext={(onClickHandler, hasNext, label) =>
@@ -53,9 +42,7 @@ const Header = () => {
                   type="button"
                   onClick={onClickHandler}
                   title={label}
-                >
-                  right
-                </button>
+                ></button>
               )
             }
             renderArrowPrev={(onClickHandler, hasPrev, label) =>
@@ -65,9 +52,7 @@ const Header = () => {
                   onClick={onClickHandler}
                   title={label}
                   className="z-10 absolute flex items-center justify-center top-1/2 -translate-y-1/2 w-[48px] h-[48px] left-0 bg-[#020512] bg-opacity-0"
-                >
-                  left
-                </button>
+                ></button>
               )
             }
           >
@@ -77,17 +62,16 @@ const Header = () => {
               objectFit="cover"
               className=" object-cover"
             />
-            <Image
-              src={sample}
-              alt="header"
-              objectFit="cover"
-              className=" object-cover"
+            <HomepageCarouselItem
+              title="SHOP NEW COLLECTION"
+              subtitle="g"
+              description="d"
+              desktopImageSrc={sample}
+              mobileImageSrc={sample}
+              buttonText="f"
+              buttonUrl="/hi"
             />
-            <div>HGEWGHAHGSG</div>
           </Carousel>
-          <div className="z-10 px-[60px] font-black text-white text-[60px]">
-            SHOP NEW COLLECTION
-          </div>
         </div>
       </div>
     </>
@@ -100,8 +84,8 @@ interface IHomepageCarousel {
   title: string | JSX.Element;
   subtitle: string;
   description: string | JSX.Element;
-  desktopImageSrc: string;
-  mobileImageSrc?: string;
+  desktopImageSrc: HTMLImageElement | string | StaticImageData;
+  mobileImageSrc?: HTMLImageElement | string | StaticImageData;
   buttonText?: string;
   buttonUrl?: string;
 }
@@ -116,41 +100,43 @@ function HomepageCarouselItem({
   buttonUrl,
 }: IHomepageCarousel) {
   return (
-    <div className="relative flex flex-col items-center max-h-[288px] h-full border border-[#1F2959] rounded-[4px] pt-[74px] pb-[76px] justify-center align-middle text-center overflow-hidden">
-      <div className="flex flex-col items-center z-10">
-        <h3 className="text-[#00E9BA] text-[10px] lg:text-sm leading-[87%] tracking-[4px] mb-2 uppercase">
+    <div className="flex flex-col items-start h-full justify-center align-middle text-center overflow-hidden">
+      <div className="flex flex-col justify-center items-start z-10 h-full">
+        {/* <h3 className="text-[#00E9BA] text-[10px] lg:text-sm leading-[87%] tracking-[4px] mb-2 uppercase">
           {subtitle}
-        </h3>
-        <h2 className="text-[32px] lg:text-[56px] leading-[%] font-bold mb-[9px] max-w-[300px] lg:max-w-none">
+        </h3> */}
+        <div className="z-10 px-[60px] font-black text-white text-[60px]">
           {title}
-        </h2>
-        <div className="text-sm lg:text-base font-normal leading-[130%] font-secondary max-w-[352px] lg:max-w-[500px] text-neutral-5 mb-[28px]">
-          {description}
         </div>
+        {/* <h2 className="text-[32px] lg:text-[56px] leading-[%] font-bold mb-[9px] max-w-[300px] lg:max-w-none">
+          {title}
+        </h2> */}
+        {/* <div className="text-sm lg:text-base font-normal leading-[130%] font-secondary max-w-[352px] lg:max-w-[500px] text-neutral-5 mb-[28px]">
+          {description}
+        </div> */}
 
-        {!!buttonText && !!buttonUrl && (
+        {/* {!!buttonText && !!buttonUrl && (
           <a href={buttonUrl} rel="noopener noreferrer" target="_blank">
             {buttonText}
           </a>
-        )}
+        )} */}
       </div>
       <Image
         src={desktopImageSrc}
-        alt="PlayCore"
+        alt="925"
         objectFit="cover"
         layout="fill"
         quality={90}
-        className="hidden lg:!block"
+        className="hidden lg:!flex h-full border"
       />
       <Image
         src={mobileImageSrc ? mobileImageSrc : desktopImageSrc}
-        alt="PlayCore"
+        alt="925"
         objectFit="cover"
         layout="fill"
         quality={90}
-        className="lg:!hidden"
+        className="lg:!hidden h-full"
       />
     </div>
   );
 }
-
