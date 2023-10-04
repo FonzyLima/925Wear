65cdd7aee81b129faf6d74db6e49faded18d7b28
Leigh Arriane Buendia <81524834+leigharriane@users.noreply.github.com>
2023-10-05 03:15:12 +0800

    added home description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Header from '@/components/Home/Header'
-import Image from 'next/image'
+import Header from "@/components/Home/Header";
+import HomeDescription from "@/components/Home/HomeDescription";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <HomeDescription
+        title="02: STONE FISH"
+        description="The newest collection honors Lobo, Batangas, the hometown of our grandparents and a second home to our family. Known for being a fishing town with scenic stone beaches and mountain ranges, 02: STONE FISH features outdoor and fishing-inspired wear to be worn in and out of the city."
+      />
     </div>
-  )
+  );
 }
