2752bc16e0f31ed93fe51833d9cc29872f18e1ce
Leigh Arriane Buendia <81524834+leigharriane@users.noreply.github.com>
2023-10-06 04:34:22 +0800

    transfered card slider file

--- src/components/Home/HomeShop.tsx
+++ src/components/Home/HomeShop.tsx
@@ -1,4 +1,7 @@
-import CardSlider from "./CardSlider";
+"use client";
+
+import { useState } from "react";
+import Card from "@/components/Shared/Card";
 
 const cards = [
   { title: "Card 1", description: "Description 1" },
@@ -17,3 +20,44 @@ const HomeShop = () => {
   );
 };
 export default HomeShop;
+
+interface CardProps {
+  title: string;
+  description: string;
+}
+
+interface CardSliderProps {
+  cards: CardProps[];
+}
+
+const CardSlider: React.FC<CardSliderProps> = ({ cards }) => {
+  const [visibleIndices, setVisibleIndices] = useState([0, 1, 2, 3]);
+
+  const nextCard = () => {
+    setVisibleIndices((prevIndices) =>
+      prevIndices.map((index) => (index + 1) % cards.length)
+    );
+  };
+
+  const prevCard = () => {
+    setVisibleIndices((prevIndices) =>
+      prevIndices.map((index) => (index === 0 ? cards.length - 1 : index - 1))
+    );
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <button onClick={prevCard} className="text-2xl mr-4">
+        &larr;
+      </button>
+      <div className="flex space-x-4">
+        {visibleIndices.map((index) => (
+          <Card key={index} {...cards[index]} />
+        ))}
+      </div>
+      <button onClick={nextCard} className="text-2xl ml-4">
+        &rarr;
+      </button>
+    </div>
+  );
+};
