f3ee60fc0857571ac5595cc370b1701789c7a8d5
Leigh Arriane Buendia <81524834+leigharriane@users.noreply.github.com>
2023-10-06 20:05:56 +0800

    finished home shop collection

--- src/components/Home/HomeShop.tsx
+++ src/components/Home/HomeShop.tsx
@@ -1,89 +1,120 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "@/components/Shared/Card";
-
-const cards = [
-  { title: "Card 1", description: "Description 1" },
-  { title: "Card 2", description: "Description 2" },
-  { title: "Card 3", description: "Description 3" },
-  { title: "Card 4", description: "Description 4" },
-  { title: "Card 5", description: "Description 4" },
-  { title: "Card 6", description: "Description 4" },
-];
-
-interface CardProps {
-  title: string;
-  description: string;
-}
-
-interface CardSliderProps {
-  cards: CardProps[];
-  visibleIndices: number[];
-  onPrevCard: () => void;
-  onNextCard: () => void;
-}
-
-const CardSlider: React.FC<CardSliderProps> = ({
-  cards,
-  visibleIndices,
-  onPrevCard,
-  onNextCard,
-}) => {
-  return (
-    <div className="flex relative items-center justify-center w-full overflow-visible">
-      <div className="flex flex-row gap-[20px] w-full">
-        {visibleIndices.map((index) => (
-          <Card key={index} {...cards[index]} />
-        ))}
-      </div>
-    </div>
-  );
-};
+import sample from "@/assets/sample.png";
+import Button from "../Shared/Button";
 
 const HomeShop = () => {
-  const [visibleIndices, setVisibleIndices] = useState([0, 1, 2, 3]);
+  const [currentPosition, setCurrentPosition] = useState(0);
+  const [currentMargin, setCurrentMargin] = useState(0);
+  const [slidesPerPage, setSlidesPerPage] = useState(0);
+  const [slidesCount, setSlidesCount] = useState(0);
 
-  const nextCard = () => {
-    setVisibleIndices((prevIndices) =>
-      prevIndices.map((index) => (index + 1) % cards.length)
-    );
+  useEffect(() => {
+    // Update the number of slides and margins on resize
+    const checkWidth = () => {
+      const containerWidth = window.innerWidth;
+      setParams(containerWidth);
+    };
+
+    window.addEventListener("resize", checkWidth);
+    checkWidth();
+
+    return () => {
+      window.removeEventListener("resize", checkWidth);
+    };
+  }, []);
+
+  const setParams = (w: number) => {
+    let newSlidesPerPage = 1;
+    if (w >= 551 && w < 901) newSlidesPerPage = 2;
+    else if (w >= 901 && w < 1101) newSlidesPerPage = 3;
+    else if (w >= 1101) newSlidesPerPage = 4;
+
+    setSlidesPerPage(newSlidesPerPage);
+    setSlidesCount(slides - newSlidesPerPage);
+
+    if (currentPosition > slidesCount) {
+      setCurrentPosition(currentPosition - newSlidesPerPage);
+    }
+
+    const newCurrentMargin = -currentPosition * (100 / newSlidesPerPage);
+    setCurrentMargin(newCurrentMargin);
   };
 
-  const prevCard = () => {
-    setVisibleIndices((prevIndices) =>
-      prevIndices.map((index) => (index === 0 ? cards.length - 1 : index - 1))
-    );
+  const slideRight = () => {
+    if (currentPosition !== 0) {
+      const newMargin = currentMargin + 100 / slidesPerPage;
+      setCurrentMargin(newMargin);
+      setCurrentPosition(currentPosition - 1);
+    }
   };
+
+  const slideLeft = () => {
+    if (currentPosition !== slidesCount) {
+      const newMargin = currentMargin - 100 / slidesPerPage;
+      setCurrentMargin(newMargin);
+      setCurrentPosition(currentPosition + 1);
+    }
+  };
+
+  const slides = 10; // Update with the actual number of slides
 
   return (
     <>
-      <div className="w-full bg-beige flex items-center justify-center p-[100px] flex-col">
+      <div className="w-full bg-beige flex items-center justify-center px-[100px] py-[55px] gap-[40px] flex-col">
         <div className="flex flex-row justify-between w-full items-center">
           <div className="text-[60px] font-bold text-black">
             EXPLORE THE COLLECTION
           </div>
           <div className="flex flex-row gap-[10px]">
             <button
-              onClick={prevCard}
-              className="text-[40px] bg-yellowgreen rounded-full w-[78px] h-[78px]"
+              onClick={slideRight}
+              className={`btn text-[40px] bg-yellowgreen rounded-full w-[78px] h-[78px] ${
+                currentPosition === 0 ? "inactive" : ""
+              }`}
             >
               &larr;
             </button>
             <button
-              onClick={nextCard}
-              className="text-[40px] bg-yellowgreen rounded-full w-[78px] h-[78px]"
+              onClick={slideLeft}
+              className={`text-[40px] bg-yellowgreen rounded-full w-[78px] h-[78px] btn ${
+                currentPosition === slidesCount ? "inactive" : ""
+              }`}
             >
               &rarr;
             </button>
           </div>
         </div>
-        <CardSlider
-          cards={cards}
-          visibleIndices={visibleIndices}
-          onPrevCard={prevCard}
-          onNextCard={nextCard}
-        />
+        <div className="container">
+          <div className="slider-container">
+            <div className="slider" style={{ marginLeft: `${currentMargin}%` }}>
+              {Array.from({ length: slides }, (_, index) => (
+                <>
+                  {/* <div key={index} className="slide">
+                  <span>{index + 1}</span>
+                   </div> */}
+                  <div key={index} className="slide">
+                    <Card
+                      image={sample}
+                      price="P 1,400"
+                      title={`Knot Skirt ${index}`}
+                    />
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex items-center justify-center">
+          <Button
+            text="Shop Now"
+            classN="bg-yellowgreen"
+            textClass="text-[22px] font-primary font-bold"
+            link="/store"
+          />
+        </div>
       </div>
     </>
   );
--- src/components/Shared/Card.tsx
+++ src/components/Shared/Card.tsx
@@ -1,13 +1,28 @@
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+
 interface CardProps {
-  title: string;
-  description: string;
+  title: string | number;
+  price: string;
+  image: HTMLImageElement | string | StaticImageData;
 }
 
-const Card: React.FC<CardProps> = ({ title, description }) => {
+const Card = ({ title, price, image }: CardProps) => {
   return (
-    <div className="bg-white p-4 shadow-md rounded-md w-[330px]">
-      <h2 className="text-lg font-semibold mb-2">{title}</h2>
-      <p>{description}</p>
+    <div className="w-full flex flex-col font-primary">
+      <div className="w-full h-[250px]">
+        <Image
+          src={image}
+          alt="item"
+          // objectFit="cover"
+          // layout="fill"
+          className="rounded-[30px] object-cover h-full"
+        />
+      </div>
+      <div className="py-[10px] px-[14px]">
+        <div className="text-[22px] font-medium">{title}</div>
+        <div className="text-[20px] font-normal">{price}</div>
+      </div>
     </div>
   );
 };
