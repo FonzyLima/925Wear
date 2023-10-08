8a78eb8238da08615494249ccf7586d4858bcc19
Leigh Arriane Buendia <81524834+leigharriane@users.noreply.github.com>
2023-10-08 20:11:38 +0800

    hide/show nav on scroll

--- src/components/Shared/Nav.tsx
+++ src/components/Shared/Nav.tsx
@@ -1,8 +1,42 @@
+"use client";
+import { useEffect, useState } from "react";
 import Link from "next/link";
+
 const Nav = () => {
+  const [scrollY, setScrollY] = useState(0);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [scrollDirection, setScrollDirection] = useState("down");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      setScrollY(currentScrollY);
+
+      if (currentScrollY > lastScrollY) {
+        setScrollDirection("down");
+      } else {
+        setScrollDirection("up");
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  const hideNavbar = scrollY > 100 && scrollDirection === "down";
+
   return (
     <>
-      <nav className="z-20 w-full fixed bg-beige px-[40px] flex flex-row justify-between items-center">
+      <div
+        className={`z-20 w-full fixed bg-beige px-[40px] flex flex-row justify-between items-center transition-transform duration-300 ease-in-out ${
+          hideNavbar ? "hidden-nav" : ""
+        }`}
+      >
         <div className="tracking-[-8.2px] text-yellowgreen font-bold text-[40px] font-brand">
           925
         </div>
@@ -17,7 +51,7 @@ const Nav = () => {
             <Link href="/store">Store</Link>
           </li>
         </ul>
-      </nav>
+      </div>
     </>
   );
 };
