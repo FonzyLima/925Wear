931446ab2aa465e2fbb300f7dcc5b78c5ba6f6e2
Leigh Arriane Buendia <81524834+leigharriane@users.noreply.github.com>
2023-10-09 21:50:03 +0800

    card hover

--- src/components/Shared/Card.tsx
+++ src/components/Shared/Card.tsx
@@ -1,29 +1,33 @@
 import { StaticImageData } from "next/image";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CardProps {
   title: string | number;
   price: string;
   image: HTMLImageElement | string | StaticImageData;
+  link?: string;
 }
 
-const Card = ({ title, price, image }: CardProps) => {
+const Card = ({ title, price, image, link = "" }: CardProps) => {
   return (
-    <div className="w-full flex flex-col font-primary">
-      <div className="w-full h-[250px]">
-        <Image
-          src={image}
-          alt="item"
-          // objectFit="cover"
-          // layout="fill"
-          className="rounded-[30px] object-cover h-full"
-        />
+    <Link href={link} className="group">
+      <div className="w-full flex flex-col font-primary ">
+        <div className="w-full h-[250px]">
+          <Image
+            src={image}
+            alt="item"
+            // objectFit="cover"
+            // layout="fill"
+            className="rounded-[30px] object-cover h-full group-hover:rounded-[70px] duration-300"
+          />
+        </div>
+        <div className="py-[10px] px-[14px]">
+          <div className="text-[22px] font-medium">{title}</div>
+          <div className="text-[20px] font-normal">{price}</div>
+        </div>
       </div>
-      <div className="py-[10px] px-[14px]">
-        <div className="text-[22px] font-medium">{title}</div>
-        <div className="text-[20px] font-normal">{price}</div>
-      </div>
-    </div>
+    </Link>
   );
 };
 
